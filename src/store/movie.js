25f68554ed253ea1af8354c95e83ab6a082da1b9
hxxeox <df21234@naver.com>
2023-02-06 11:44:14 +0900

    Create project

--- src/store/movie.js
+++ src/store/movie.js
@@ -64,8 +64,7 @@ export default {
                         })
                     }
                 }
-            }
-            catch(message){
+            } catch({message}) {
                 commit('updateState', {
                     movies:[],
                     message
@@ -88,7 +87,7 @@ export default {
                 commit('updateState', {
                     theMovie:res.data
                 })
-            }catch (error) {
+            } catch (error) {
                 commit('updateState', {
                     theMovie:{}
                 })
@@ -102,23 +101,6 @@ export default {
     },
 }
 
-function _fetchMovie(payload) {
-    const { title, type, year, page, id } = payload
-    const OMDB_API_KEY = '7035c60c'
-    const url = id 
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-    return new Promise((resolve, reject)=>{
-        axios.get(url)
-         .then(res =>{
-            if(res.data.Error) {
-                reject(res.data.Error)
-            }
-            resolve(res)
-         })
-         .catch(err => {
-            reject(err.message)
-         })
-    })
+async function _fetchMovie(payload) {
+    return await axios.post('/.natlify/functions/movie',payload)
 }
